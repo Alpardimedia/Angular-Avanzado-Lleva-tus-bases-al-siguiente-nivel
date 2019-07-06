bf28dece067b2383c862c6c7c17ccf44177b474b
Alberto Pardo Díaz <albertopardo21690@gmail.com>
2019-07-06 18:21:43 +0200

    Realización de promesas y observables

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,17 +4,21 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { PromesasComponent } from './promesas/promesas.component';
 import { AccountSettingsComponent } from './account-settings/acount-settings.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const pageRoutes: Routes = [
     { 
         path: '', 
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: 'account-settings', component: AccountSettingsComponent },
+            { path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+            { path: 'progress', component: ProgressComponent, data: {titulo: 'Barra de progreso'} },
+            { path: 'graficas1', component: Graficas1Component, data: {titulo: 'Gráficas'} },
+            { path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
+            { path: 'rxjs', component: RxjsComponent, data: {titulo: 'RxJs'} },
+            { path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Opciones de la cuenta'} },
             { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
         ]
     }
